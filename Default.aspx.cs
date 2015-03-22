9050fb52e495cf23f1ed46c3b380bfb070afe566
Jose Contreras <jocontre@joselcontreras.com>
2015-03-22 17:38:37 -0600

    small change

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,10 +9,9 @@ using System.Data.SqlClient;
 using System.Data.Sql;
 using System.Configuration; /// Extracts values from Web.config when connection is made.
 
-/// <summary>
 /// This file is for the button Save.
 /// Interesting that C# uses 3 forward slashes for comments.
-/// </summary>
+
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
